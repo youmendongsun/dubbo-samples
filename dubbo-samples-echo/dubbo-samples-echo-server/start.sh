1dbd20eedcf1404665c4cab3e2be0f1dfe1f81e2
Jason Song <songshun.ss@antfin.com>
2019-09-08 20:29:35 +0800

    fix spring boot issue

--- dubbo-samples-echo/dubbo-samples-echo-server/start.sh
+++ dubbo-samples-echo/dubbo-samples-echo-server/start.sh
@@ -7,4 +7,4 @@ export SOFA_INSTANCE_ID=NY24NMCS0M9J
 export SOFA_ACCESS_KEY=cPYkbIXrZHjVAxjxhm
 export SOFA_SECRET_KEY=7F5k7YvUokWDAf94OhBz6Pes6W3UAGBO
 
-java -Dcom.alipay.env=shared -Dcom.antcloud.antvip.endpoint=${SOFA_ANTVIP_ENDPOINT} -Dcom.alipay.instanceid=${SOFA_INSTANCE_ID} -Dcom.antcloud.mw.access=$SOFA_ACCESS_KEY -Dcom.antcloud.mw.secret=$SOFA_SECRET_KEY -Dmesh_http_read_timeout=600000 -Duser.timezone=Asia/Shanghai -jar /home/admin/release/dubbo-samples-echo-server-1.0-SNAPSHOT.jar
+java -DMOSN_ENABLE=true -Dcom.alipay.env=shared -Dcom.antcloud.antvip.endpoint=${SOFA_ANTVIP_ENDPOINT} -Dcom.alipay.instanceid=${SOFA_INSTANCE_ID} -Dcom.antcloud.mw.access=$SOFA_ACCESS_KEY -Dcom.antcloud.mw.secret=$SOFA_SECRET_KEY -Dmesh_http_read_timeout=600000 -Duser.timezone=Asia/Shanghai -jar /home/admin/release/dubbo-samples-echo-server-1.0-SNAPSHOT.jar
